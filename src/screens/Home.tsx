4d325ad78bff81ff17da82b6bc98713907cf39e7
Giovanna Di Mauro <giovydimauro@live.it>
2025-10-22 11:58:15 +0200

    Remove HomeBody component from Home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,13 @@
 import { FloatingButton } from "../components/FloatingButton";
 import Footer from "../components/Footer";
 import { Header } from "../components/Header";
-import { HomeBody } from "../components/HomeBody";
 
 export function Home() {
   return (
     <div className="flex flex-col min-h-screen pt-14 space-y-30">
       <Header />
       <main className="flex-grow">
-        <HomeBody theme="light"/>
+
         <FloatingButton />
       </main>
       <Footer />
